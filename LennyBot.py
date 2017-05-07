65a4a307c74964b75c7fff3ba0dfa8d026b39ca9
Isklar <mrisklar@gmail.com>
2017-05-07 12:22:55 +0100

    Add server join/leave notifications to log

--- LennyBot.py
+++ LennyBot.py
@@ -3,6 +3,7 @@ import asyncio
 import credentials
 
 client = discord.Client()
+logChannel = '310083179052793858' #bot_log
 
 @client.event
 async def on_ready():
@@ -29,6 +30,12 @@ async def on_message(message):
                     numServers+=1
 
                 await client.send_message(message.channel, str(numServers) + ' servers.')
+@client.event
+async def on_server_join(server):
+   await client.send_message(client.get_channel(logChannel), ':heart: Lenny was added to ' + str(server) + ' - ' + str(sum(1 for e in server.members)))
 
+@client.event
+async def on_server_remove(server):
+   await client.send_message(client.get_channel(logChannel), ':broken_heart: Lenny was removed from ' + str(server))
 
 client.run(credentials.token)
