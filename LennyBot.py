150bbe0f5863fd11092fc960c8b72a4e9c104c33
Isklar <mrisklar@gmail.com>
2017-05-07 12:31:01 +0100

    Remove command from PM and added better logging and total count.
    The owner can now just post in any channel and the bot will output to the log channel. Any PMs sent to the bot will just display a help message. The bot's status message will now display a total count of lennyfaces used.

--- LennyBot.py
+++ LennyBot.py
@@ -11,25 +11,47 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    #await client.change_presence(game=discord.Game(name='@Lenny enabled')) # simple news/update status
 
 @client.event
 async def on_message(message):
-    if 'lennyface' in message.content.lower():
+  if message.author != client.user and not message.author.bot:
+    if message.author.id == credentials.owner:
+        servers = []
+        if 'servers' in message.content.lower():
+            numServers = 0
+            for server in client.servers:
+                numServers+=1
+                servers.append(server.name)
+
+            await client.send_message(client.get_channel(logChannel), ', '.join(servers) + ' - ' + str(numServers) + ' servers, ' + str(sum(1 for _ in client.get_all_members())) + ' users.')
+
+    if message.channel.is_private:
+        await client.send_message(client.get_channel(logChannel), ':mailbox_with_mail: ' + message.author.name + ' - ' + message.clean_content)
+        await client.send_message(message.channel, 'Hello ' + message.author.name + " ( ͡° ͜ʖ ͡°). I don\'t have any commands! I\'m triggered by the phrase lennyface or via " + client.user.mention + ", have a nice day!")
+
+    ## Lennyface send / delete
+    if 'lennyface' in message.content.lower() or client.user.mentioned_in(message) and not message.mention_everyone:
         await client.send_message(message.channel, '( ͡° ͜ʖ ͡°)')
-        if  message.content.lower() == 'lennyface':
+        await client.send_message(client.get_channel(logChannel), '[' + message.author.server.name + '] ' + message.author.name + ' - ' + message.clean_content)
+
+        if (message.content.lower() == 'lennyface') or (message.content.lower() == client.user.mention):
             try:
                 await client.delete_message(message)
             except Exception as e:
                 print(e)
 
-    if message.channel.is_private:
-        if message.author.id == credentials.owner:
-            if 'servers' in message.content:
-                numServers = 0
-                for server in client.servers:
-                    numServers+=1
+        # Log lenny count
+        with open(r'count.txt','r+') as f:
+            value = int(f.read())
+            f.seek(0)
+            f.write(str(value + 1))
+            await client.change_presence(game=discord.Game(name='( ͡° ͜ʖ ͡°) - ' + str(value + 1) ))
 
-                await client.send_message(message.channel, str(numServers) + ' servers.')
+
+    elif 'lenny' in message.content.lower():
+        await client.send_message(client.get_channel(logChannel), '[' + message.author.server.name + '] ' + message.author.name + ' - ' + message.clean_content)
+
 @client.event
 async def on_server_join(server):
    await client.send_message(client.get_channel(logChannel), ':heart: Lenny was added to ' + str(server) + ' - ' + str(sum(1 for e in server.members)))
