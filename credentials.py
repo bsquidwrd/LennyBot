14891059a64891c9689616c85380fbbd9d3db190
Brandon <git@bsquid.io>
2018-01-10 09:06:02 -0800

    Delete credentials.py

--- credentials.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ['token'] = ''
-os.environ['owner'] = '1234567890'
-os.environ['logChannel'] = '1234567890'
-os.environ['dbots_key'] = ''
-os.environ['invite_url'] = 'https://discordapp.com/oauth2/authorize?client_id=193179442665750528&scope=bot&permissions=0x00002000'
